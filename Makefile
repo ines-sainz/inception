9ddb1dcbce28e7cf394627b3353049120191a041
ines-sainz <ines.sainz13@gmail.com>
2025-05-28 12:15:48 +0200

    fix makefile, add wordpress automatic config

--- Makefile
+++ Makefile
@@ -4,17 +4,19 @@ COMPOSE_FILE=srcs/docker-compose.yml
 all: up
 
 up:
-	sudo rm -rf /home/isainz-r/data/MD/*
-	sudo rm -rf /home/isainz-r/data/WP/*
 	$(COMPOSE) -f $(COMPOSE_FILE) up --build -d
 
 down:
 	$(COMPOSE) -f $(COMPOSE_FILE) down
 
 fclean:
+	sudo rm -rf /home/isainz-r/data/MD/*
+	sudo rm -rf /home/isainz-r/data/WP/*
 	$(COMPOSE) -f $(COMPOSE_FILE) down --volumes --remove-orphans
 
-remove: fclean
+remove: down
+	sudo rm -rf /home/isainz-r/data/MD/*
+	sudo rm -rf /home/isainz-r/data/WP/*
 	docker network prune
 	docker rmi $$(docker images -aq)
 
--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
 set -e
 
 echo "wordpress iniciado"
 
-#Wait for MariaDB to be ready (simple ping loop)
+# Wait for MariaDB to be ready
 until mysqladmin ping -h"$MYSQL_HOST" --silent; do
 	echo "Waiting for MariaDB..."
 	sleep 2
@@ -12,38 +11,47 @@ done
 
 mkdir -p /run/php
 
+# Copy WordPress files if not already present
 if [ ! -f /var/www/html/index.php ]; then
 	echo "Copying Wordpress to /var/www/html"
 	cp -a /tmp/wp/. /var/www/html/
 fi
 
-#if [ -z "$(ls -A /var/www/html)" ]; then
-#	echo "Copy wordpress to /var/www/html"
-#	cp -a /tmp/wp/. /var/www/html/
-#fi
+cd /var/www/html
 
-#If wp-config.php doesn't exist, generate it
-if [ ! -f /var/www/html/wp-config.php ]; then
-	echo "Setting up wp-config.php..."
+# Install WordPress using WP-CLI if not already installed
+if ! wp core is-installed --allow-root; then
+	echo "Setting up wp-config.php and installing WordPress..."
 
-	if [ ! -f /var/www/html/wp-config-sample.php ]; then
-		echo "config_error: wp-config-sample.php not found"
-		ls -la /var/www/html
-		exit 1
-	fi
+	wp config create \
+		--dbname="$MYSQL_DATABASE" \
+		--dbuser="$MYSQL_USER" \
+		--dbpass="$MYSQL_PASSWORD" \
+		--dbhost="$MYSQL_HOST" \
+		--locale=es_ES \
+		--allow-root
 
-	cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+	wp core install \
+		--url="https://$DOMAIN_NAME" \
+		--title="$WP_TITLE" \
+		--admin_user="$WP_ADMIN_USER" \
+		--admin_password="$WP_ADMIN_PASSWORD" \
+		--admin_email="$WP_ADMIN_EMAIL" \
+		--skip-email \
+		--allow-root
 
-	sed -i "s/database_name_here/${MYSQL_DATABASE}/" /var/www/html/wp-config.php
-	sed -i "s/username_here/${MYSQL_USER}/" /var/www/html/wp-config.php
-	sed -i "s/password_here/${MYSQL_PASSWORD}/" /var/www/html/wp-config.php
-	sed -i "s/localhost/${MYSQL_HOST}/" /var/www/html/wp-config.php
-	sed -i "s/false/true/" /var/www/html/wp-config.php
+	echo "Creating additional WordPress user..."
+	wp user create "$WP_USER" "$WP_USER_EMAIL" \
+		--user_pass="$WP_USER_PASSWORD" \
+		--role=author \
+		--allow-root
+else
+	echo "WordPress already installed"
 fi
 
-#Set permissions
+# Set correct permissions
 chown -R www-data:www-data /var/www/html
 
-echo "start php-fpm"
-#Start php-fpm
+echo "Starting PHP-FPM..."
 exec php-fpm7.4 -F
+
